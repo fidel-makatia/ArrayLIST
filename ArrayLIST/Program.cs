2383d1289a75611b9458a143e626f1f10139edb5
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-08 23:20:21 +0300

    Add project files.

---
+++ ArrayLIST/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayLIST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Supports any type of data type
+            ArrayList arr = new ArrayList()
+            {
+                123, "ieieiei", 56, 'A'
+            };
+            arr.Add("ff");
+
+            foreach(var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
